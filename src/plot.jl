f3830f70ca86765fefccd8e291ccaedd88b8b39b
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-09-11 14:06:26 +0000

    Snapshot of branch master taken on 2019-09-11T10:06:25.237-04:00 from "https://github.com/UnofficialJuliaMirror/NeRCA.jl-89b7c20c-a96a-11e9-35df-35fba0891eb2"

--- src/plot.jl
+++ src/plot.jl
@@ -19,7 +19,7 @@ function ztplot(hits::Vector{CalibratedHit}; du=nothing, t₀=0)
     ax = @pgf Axis({xlabel=raw"time [\si{\ns}]", ylabel=raw"z [\si{\m}]"})
     push!(ax,  @pgf PlotInc({only_marks, mark="*", mark_size=".8"}, ZTCoordinates(hits, offset=t₀)))
     push!(ax, LegendEntry("hits"))
-    push!(ax,  @pgf PlotInc({only_marks, mark="x", mark_size="4"}, ZTCoordinates(triggered_hits, offset=t₀)))
+    push!(ax,  @pgf PlotInc({only_marks, mark="+", mark_size="5"}, ZTCoordinates(triggered_hits, offset=t₀)))
     push!(ax, LegendEntry("triggered"))
     ax
 end
@@ -32,10 +32,15 @@ Create a zt-plot from hits and a ROyFit using PGFPlotsX.
 """
 function ztplot(hits::Vector{CalibratedHit}, fit::NeRCA.ROyFit; t₀=0)
     ax = ztplot(hits; t₀=t₀)
-    zs = range(0, 800, length=200)
+    zs = range(0, maximum(h->h.pos.z, hits), length=200)
     dᵧ, ccalc = NeRCA.make_cherenkov_calculator(fit.sdp)
     push!(ax, @pgf PlotInc({thick}, Coordinates(ccalc.(zs) .- t₀, zs)))
     push!(ax, LegendEntry("fit"))
+    push!(ax, @pgf PlotInc(
+        {thick, only_marks, mark="o", mark_size="4"},
+        ZTCoordinates(fit.selected_hits, offset=t₀))
+    )
+    push!(ax, LegendEntry("selected"))
     ax
 end
 
